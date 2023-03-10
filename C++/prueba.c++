4341cc406e525f5944dc8326d9a130decf08f359
DilanLM <dylanmitchell373@gmail.com>
2023-03-10 18:20:33 -0500

    Respaldo

--- C++/prueba.c++
+++ C++/prueba.c++
@@ -1,15 +1,106 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+const int HEIGHT = 20;
+const int WIDTH = 40;
+const int GAP_SIZE = 5;
+const int OBSTACLE_WIDTH = 4;
+const int GRAVITY = 1;
+const int JUMP_POWER = -4;
+
+struct Bird {
+    int x, y, yVelocity;
+};
+
+struct Obstacle {
+    int x, gapPosition;
+};
+
+void setCursorPosition(int x, int y) {
+    static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    cout.flush();
+    COORD coord = { (SHORT)x, (SHORT)y };
+    SetConsoleCursorPosition(hOut, coord);
+}
+
+void drawBird(Bird bird) {
+    setCursorPosition(bird.x, bird.y);
+    cout << "v";
+}
+
+void drawObstacle(Obstacle obstacle) {
+    for (int y = 0; y < obstacle.gapPosition; y++) {
+        setCursorPosition(obstacle.x, y);
+        cout << "|";
+    }
+    for (int y = obstacle.gapPosition + GAP_SIZE; y < HEIGHT; y++) {
+        setCursorPosition(obstacle.x, y);
+        cout << "|";
+    }
+}
+
+bool isColliding(Bird bird, Obstacle obstacle) {
+    if (bird.x >= obstacle.x && bird.x <= obstacle.x + OBSTACLE_WIDTH) {
+        if (bird.y <= obstacle.gapPosition || bird.y >= obstacle.gapPosition + GAP_SIZE) {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main() {
-   
+    srand(time(NULL));
 
-   for (int i = 0; i <= 99; i++)
-   {
-      std::cout << i << '\n';
-      
-   }
+    Bird bird = { WIDTH / 4, HEIGHT / 2, 0 };
 
-   std::cout << "hola";
-   
-   return 0;
+    Obstacle obstacles[3];
+    for (int i = 0; i < 3; i++) {
+        obstacles[i].x = WIDTH + i * (WIDTH / 3);
+        obstacles[i].gapPosition = rand() % (HEIGHT - GAP_SIZE - 2) + 1;
+    }
+
+    while (true) {
+        if (_kbhit()) {
+            char key = _getch();
+            if (key == ' ') {
+                bird.yVelocity = JUMP_POWER;
+            }
+        }
+
+        bird.yVelocity += GRAVITY;
+        bird.y += bird.yVelocity;
+
+        if (bird.y < 0 || bird.y >= HEIGHT) {
+            cout << "Game over!" << endl;
+            return 0;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            obstacles[i].x--;
+
+            if (obstacles[i].x < -OBSTACLE_WIDTH) {
+                obstacles[i].x = WIDTH;
+                obstacles[i].gapPosition = rand() % (HEIGHT - GAP_SIZE - 2) + 1;
+            }
+
+            if (isColliding(bird, obstacles[i])) {
+                cout << "Game over!" << endl;
+                return 0;
+            }
+
+            drawObstacle(obstacles[i]);
+        }
+
+        drawBird(bird);
+
+        Sleep(100);
+        system("cls");
+    }
+
+    return 0;
 }
--- JAVASCRIPT/aprendiendoJS.js
+++ JAVASCRIPT/aprendiendoJS.js
@@ -1,59 +1,102 @@
-function variables() {
-  let variable1;
-  var variable2;
-  const constante = [];
+// Configuración inicial
+let canvasWidth = 800;
+let canvasHeight = 600;
+
+let backgroundColor = 50;
+let currentColor = 255;
+let currentSize = 20;
+
+let shapes = [];
+
+// Función para crear el canvas
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+  background(backgroundColor);
 }
 
-function condicionales() {
-  let numero = 3;
-  let numero2 = 4;
-  let suma = numero + numero2;
+// Función para dibujar en el canvas
+function draw() {
+  background(backgroundColor);
 
-  if (suma < 8) {
-    console.log("Esta mal pero no tanto el resultado");
-  } else if (suma == 7) {
-    console.log("Esta bien el resultado");
-  } else {
-    console.log("ahora si esta cule malo el resultado");
+  // Dibujar todas las figuras almacenadas
+  for (let i = 0; i < shapes.length; i++) {
+    let shape = shapes[i];
+    stroke(shape.color);
+    strokeWeight(shape.size);
+    noFill();
+    if (shape.type == "circle") {
+      ellipse(shape.x, shape.y, shape.size);
+    } else if (shape.type == "rectangle") {
+      rect(shape.x, shape.y, shape.size, shape.size);
+    } else if (shape.type == "triangle") {
+      triangle(
+        shape.x,
+        shape.y - shape.size / 2,
+        shape.x - shape.size / 2,
+        shape.y + shape.size / 2,
+        shape.x + shape.size / 2,
+        shape.y + shape.size / 2
+      );
+    }
+  }
+
+  // Dibujar figura actual
+  stroke(currentColor);
+  strokeWeight(currentSize);
+  noFill();
+  if (mouseIsPressed) {
+    if (keyIsDown(67)) {
+      // 'C' para dibujar un círculo
+      ellipse(mouseX, mouseY, currentSize);
+      shapes.push({
+        type: "circle",
+        x: mouseX,
+        y: mouseY,
+        size: currentSize,
+        color: currentColor,
+      });
+    } else if (keyIsDown(82)) {
+      // 'R' para dibujar un rectángulo
+      rect(mouseX, mouseY, currentSize, currentSize);
+      shapes.push({
+        type: "rectangle",
+        x: mouseX,
+        y: mouseY,
+        size: currentSize,
+        color: currentColor,
+      });
+    } else if (keyIsDown(84)) {
+      // 'T' para dibujar un triángulo
+      triangle(
+        mouseX,
+        mouseY - currentSize / 2,
+        mouseX - currentSize / 2,
+        mouseY + currentSize / 2,
+        mouseX + currentSize / 2,
+        mouseY + currentSize / 2
+      );
+      shapes.push({
+        type: "triangle",
+        x: mouseX,
+        y: mouseY,
+        size: currentSize,
+        color: currentColor,
+      });
+    }
   }
 }
 
-function bucles() {
-  // while
-  for (let i = 0; i < 13; i++) {
-    console.log(i);
+// Función para cambiar el tamaño de la figura actual
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    currentSize += 5;
+  } else if (keyCode == DOWN_ARROW) {
+    currentSize -= 5;
   }
+  currentSize = constrain(currentSize, 5, 100);
 }
 
-function funciones(num1, num2) {
-  let resultado = num1 * num2;
-  return resultado;
-}
-
-function arrays() {
-  const array = [0, 1, 2, 3, 4];
-
-  for (let i = 0; i <= 4; i++) {
-    console.log("numero del indice" + i);
-    let mostrar = array[i];
-    console.log(mostrar);
-  }
-}
-
-function objetos() {
-  let persona = {
-    nombre: "David",
-    edad: 24,
-    sexo: "masculino",
-    pareja: true,
-  };
-
-  let persona2 = {
-    nombre: "Daniela",
-    edad: 27,
-    sexo: "femenino",
-    pareja: false,
-  };
-
-  return console.log((persona.talla = "Talla M"));
+// Función para cambiar el color de la figura actual
+function mouseClicked() {
+  currentColor = color(random(255), random(255), random(255));
 }
